44613f50e23a2edcf4e09c1a602148099cf46ec1
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-23 11:25:16 +0100

    added french support

--- hello/hello.go
+++ hello/hello.go
@@ -1,11 +1,11 @@
 package main
 
 import "fmt"
-
-
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
     if name == "" {
@@ -16,9 +16,12 @@ func Hello(name string, language string) string {
         return spanishHelloPrefix + name
     }
 
+    if language == french {
+        return frenchHelloPrefix + name
+    }
+
     return englishHelloPrefix + name
 }
-
 
 func main() {
 	fmt.Println( Hello("world","") )
