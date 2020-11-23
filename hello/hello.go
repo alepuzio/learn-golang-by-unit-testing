2ab0a619d8b4dff6611b9137dfc7b0ae5f82e960
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-23 11:19:11 +0100

    refacting spanish

--- hello/hello.go
+++ hello/hello.go
@@ -3,16 +3,20 @@ package main
 import "fmt"
 
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string, language string ) string {
-	if name == "" {
-		name = "World"
-	}
-	if language == "Spanish" {
-        	return "Hola, " + name
-    	}
-	return englishHelloPrefix + name
+func Hello(name string, language string) string {
+    if name == "" {
+        name = "World"
+    }
+
+    if language == spanish {
+        return spanishHelloPrefix + name
+    }
+
+    return englishHelloPrefix + name
 }
 
 
