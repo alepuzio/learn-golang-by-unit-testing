ad727fc70f05ec0997535f4ab6a64b56a913587b
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-23 11:28:26 +0100

    refactoring

--- hello/hello.go
+++ hello/hello.go
@@ -27,7 +27,26 @@ func Hello(name string, language string) string {
 func main() {
 	fmt.Println( Hello("world","") )
 }
+func Hello(name string, language string) string {
+    if name == "" {
+        name = "World"
+    }
 
+    return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+    switch language {
+    case french:
+        prefix = frenchHelloPrefix
+    case spanish:
+        prefix = spanishHelloPrefix
+    default:
+        prefix = englishHelloPrefix
+    }
+    return
+}
+/*first version of switch
 func Hello(name string, language string) string {
     if name == "" {
         name = "World"
@@ -44,3 +63,4 @@ func Hello(name string, language string) string {
 
     return prefix + name
 }
+*/
