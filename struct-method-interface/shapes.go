a186783f7ab52e75eccc8e0c3f253b27d5495090
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-26 11:48:29 +0100

    success test with type shape

--- struct-method-interface/shapes.go
+++ struct-method-interface/shapes.go
@@ -1,19 +1,85 @@
 package main
 
-
-
+import "math"
 
 type Rectangle struct {
-    Width float64
+    Width  float64
     Height float64
 }
 
-
-func Perimeter(rectangle Rectangle) float64 {
-    return 2 * (rectangle.Width + rectangle.Height)
+func (r Rectangle) Area() float64  {
+    return r.Width * r.Height
+}
+type Circle struct {
+    Radius float64
+}
+func (c Circle) Area() float64  {
+    return math.Pi * c.Radius * c.Radius
 }
 
-func Area(rectangle Rectangle) float64 {
-    return rectangle.Width * rectangle.Height
+
+type Shape interface {
+    Area() float64
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
