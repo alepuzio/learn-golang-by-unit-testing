e66de6e319e23423cafa1a3af3e283f526cde264
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-26 12:30:08 +0100

    introduce table data tests

--- struct-method-interface/shapes_test.go
+++ struct-method-interface/shapes_test.go
@@ -3,7 +3,14 @@ package main
 import "testing"
 
 
+/*using interface
+it's not necessary that the class declare implemention
 
+the type can implement a method in the interface
+when the method is used referring the interface,
+the compiler create autmatically a link
+between the type and the interface
+doubt: how about a method with same name and more than one interface?"
 func TestArea(t *testing.T) {
 
     checkArea := func(t *testing.T, shape Shape, want float64) {
@@ -25,8 +32,30 @@ func TestArea(t *testing.T) {
     })
 
 }
+**/
 
+/**
+table driven tests
+*/
+func TestArea(t *testing.T) {
 
+    areaTests := []struct {
+        shape Shape
+        want  float64
+    }{
+        {Rectangle{12, 6}, 72.0},
+        {Circle{10}, 314.1592653589793},
+    }
+
+    for _, tt := range areaTests {
+        got := tt.shape.Area()
+        print(got)
+	if got != tt.want {
+            t.Errorf("got %g want %g", got, tt.want)
+        }
+    }
+
+}
 
 
 
