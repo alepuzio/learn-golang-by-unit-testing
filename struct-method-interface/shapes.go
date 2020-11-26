4b2208260bd1b2e7f06453474465276bfcbec07f
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-26 11:26:05 +0100

    added Area

--- struct-method-interface/shapes.go
+++ struct-method-interface/shapes.go
@@ -1,5 +1,10 @@
 package main
 
+
 func Perimeter(width float64, height float64) float64 {
     return 2 * (width + height)
+}
+
+func Area(width float64, height float64) float64 {
+    return width * height
 }
