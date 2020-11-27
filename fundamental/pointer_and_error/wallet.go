ded89778f03dff47736525507eb7d00351ffd1f1
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-27 09:57:25 +0100

    different addresses

--- fundamental/pointer_and_error/wallet.go
+++ fundamental/pointer_and_error/wallet.go
@@ -1,15 +1,15 @@
 package main
 
-
+import "fmt"
 
 type Wallet struct {
 	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
-	w.balance += amount
+    fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+    w.balance += amount
 }
-
 func (w Wallet) Balance() int {
-    return 0
+    return w.balance
 }
