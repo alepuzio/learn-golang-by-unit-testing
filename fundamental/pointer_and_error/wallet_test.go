2124fa583fffe7250cd1987ea32e6225e789d2d8
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-27 10:10:07 +0100

    fixed test, equal addresses

--- fundamental/pointer_and_error/wallet_test.go
+++ fundamental/pointer_and_error/wallet_test.go
@@ -8,10 +8,15 @@ import "fmt"
 func TestWallet(t *testing.T) {
 
     wallet := Wallet{}
+
     wallet.Deposit(10)
+
     got := wallet.Balance()
+
     fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+
     want := 10
+
     if got != want {
         t.Errorf("got %d want %d", got, want)
     }
