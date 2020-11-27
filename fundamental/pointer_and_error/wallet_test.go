ebab55a16830016a52fa4c5381d89f510e1ff2cd
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-27 11:08:44 +0100

    refactroing

--- fundamental/pointer_and_error/wallet_test.go
+++ fundamental/pointer_and_error/wallet_test.go
@@ -2,66 +2,49 @@ package main
 
 import "testing"
 
-
 func TestWallet(t *testing.T) {
 
-    assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
-        t.Helper()
-        got := wallet.Balance()
-
-        if got != want {
-            t.Errorf("got %s want %s", got, want)
-        }
-    }//end assertBalance
-
-assertError := func(t *testing.T, got error, want string) {
-    t.Helper()
-    if got == nil {
-        t.Fatal("didn't get an error but wanted one")
-    }
-
-    if got.Error() != want {
-        t.Errorf("got %q, want %q", got, want)
-    }
-}
-
-/***
-when I run go test, the order of implementation of the func
-does matter.
-I can't invoke a func in the row k-th if the implementation 
-is after
-*/
-t.Run("Deposit", func(t *testing.T) {
+    t.Run("Deposit", func(t *testing.T) {
         wallet := Wallet{}
         wallet.Deposit(Bitcoin(10))
         assertBalance(t, wallet, Bitcoin(10))
     })
 
-    t.Run("Withdraw", func(t *testing.T) {
-        wallet := Wallet{balance: Bitcoin(20)}
+    t.Run("Withdraw with funds", func(t *testing.T) {
+        wallet := Wallet{Bitcoin(20)}
         wallet.Withdraw(Bitcoin(10))
         assertBalance(t, wallet, Bitcoin(10))
     })
 
+    t.Run("Withdraw insufficient funds", func(t *testing.T) {
+        wallet := Wallet{Bitcoin(20)}
+        err := wallet.Withdraw(Bitcoin(100))
 
-t.Run("Withdraw insufficient funds", func(t *testing.T) {
-    startingBalance := Bitcoin(20)
-    wallet := Wallet { startingBalance }
-    err := wallet.Withdraw( Bitcoin(100) )
+        assertBalance(t, wallet, Bitcoin(20))
+        assertError(t, err, ErrInsufficientFunds)
+    })
+}
 
-    assertBalance(t, wallet, startingBalance)
-    assertError(t, err, "cannot withdraw, insufficient funds")
-})
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+    t.Helper()
+    got := wallet.Balance()
 
+    if got != want {
+        t.Errorf("got %q want %q", got, want)
+    }
+}
 
+func assertError(t *testing.T, got error, want error) {
+    t.Helper()
+    if got == nil {
+        t.Fatal("didn't get an error but wanted one")
+    }
 
+    if got != want {
+        t.Errorf("got %q, want %q", got, want)
+    }
 
-
-
-}//end func
-
-
-
+}
 
 
 
