7468a81b5215aca34ff74736e2440bf653a50f7a
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-30 15:37:51 +0100

    refactoring

--- fundamental/map/dictionary_test.go
+++ fundamental/map/dictionary_test.go
@@ -2,13 +2,26 @@ package main
 
 import "testing"
 
+/**
+test
+*/
 func TestSearch(t *testing.T) {
     dictionary := map[string]string{"test": "this is just a test"}
 
     got := Search(dictionary, "test")
     want := "this is just a test"
 
+    assertStrings(t, got, want)
+}
+
+
+/**
+centralize assert
+*/
+func assertStrings(t *testing.T, got, want string) {
+    t.Helper()
+
     if got != want {
-        t.Errorf("got %q want %q given, %q", got, want, "test")
+        t.Errorf("got %q want %q", got, want)
     }
 }
