cce529a35ff47486ba0376354527fefc306acb83
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-12-01 17:24:17 +0100

    updated readme and first sucessfull test

---
+++ fundamental/mocking/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// Countdown prints a countdown from 3 to out.
+func Countdown(out io.Writer) {
+	fmt.Fprint(out, "3")
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
+
