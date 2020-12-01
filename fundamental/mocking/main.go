fe165d42c7e4535272643b11744bc4a127f311c2
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-12-01 17:37:09 +0100

    refactoring

--- fundamental/mocking/main.go
+++ fundamental/mocking/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 // Countdown prints a countdown from 3 to out.
+
+
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-    for i := 3; i > 0; i-- {
+    for i := countdownStart; i > 0; i-- {
         fmt.Fprintln(out, i)
     }
-    fmt.Fprint(out, "Go!")
+    
+    time.Sleep(1 * time.Second)
+    fmt.Fprint(out, finalWord)
 }
