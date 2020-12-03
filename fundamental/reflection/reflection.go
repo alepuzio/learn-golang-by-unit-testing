2a5c444a9f2c441e84bf2fc967d00ec62c91ad15
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-12-03 11:32:46 +0100

    sucessful ltest with Elem(), I stop this section of the turoail, it s borign

--- fundamental/reflection/reflection.go
+++ fundamental/reflection/reflection.go
@@ -4,17 +4,26 @@ import (
 	"reflect"
 )
 
+
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
+    val := reflect.ValueOf(x)
+    /*
+    You can't use NumField on a pointer Value, 
+    we need to extract the underlying value 
+    before we can do that by using Elem()
+    */
+    if val.Kind() == reflect.Ptr {
+        val = val.Elem()
+    }
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
+    for i := 0; i < val.NumField(); i++ {
+        field := val.Field(i)
 
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
-	}
+        switch field.Kind() {
+        case reflect.String:
+            fn(field.String())
+        case reflect.Struct:
+            walk(field.Interface(), fn)
+        }
+    }
 }
