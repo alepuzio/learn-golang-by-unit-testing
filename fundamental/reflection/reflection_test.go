b08efaa17b5637826207e98ad69bcbe3a0d1bfdd
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-12-03 11:26:24 +0100

    sucessfull test swich and kind()

--- fundamental/reflection/reflection_test.go
+++ fundamental/reflection/reflection_test.go
@@ -1,38 +1,68 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
-
 
 func TestWalk(t *testing.T) {
 
-    cases := []struct{
-        Name string
-        Input interface{}
-        ExpectedCalls []string
-    } {
-        {
-            "Struct with one string field",
-            struct {
-                Name string
-		Age int
-            }{ "Chris",32},
-            []string{"Chris"},
-        },
-    }
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct{ Name string }{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+	}
 
-    for _, test := range cases {
-        t.Run(test.Name, func(t *testing.T) {
-            var got []string
-            walk(test.Input, func(input string) {
-                got = append(got, input)
-            })
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
 
-            if !reflect.DeepEqual(got, test.ExpectedCalls) {
-                t.Errorf("got %v, want %v", got, test.ExpectedCalls)
-            }
-        })
-    }
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
 }
