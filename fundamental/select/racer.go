3a01798dfdd00a169df22149381eaa9dbf0e365c
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-12-03 10:08:24 +0100

    removed commented code

--- fundamental/select/racer.go
+++ fundamental/select/racer.go
@@ -1,39 +1,16 @@
 package main
 import (
 	"net/http"
-//	"net/http/httptest"
 	"time"
 	"fmt"
 )
 
 
-/*
-func Racer(a, b string) (winner string) {
-    aDuration := measureResponseTime(a)
-    bDuration := measureResponseTime(b)
-
-    if aDuration < bDuration {
-        return a
-    }
-
-    return b
-}
-*/
 func measureResponseTime(url string) time.Duration {
     start := time.Now()
     http.Get(url)
     return time.Since(start)
 }
-/*
-func Racer(a, b string) (winner string) {
-    select {
-    case <-ping(a):
-        return a
-    case <-ping(b):
-        return b
-    }
-}
-*/
 func ping(url string) chan struct{} {
     ch := make(chan struct{})
     go func() {
@@ -43,14 +20,28 @@ func ping(url string) chan struct{} {
     return ch
 }
 
+var tenSecondTimeout = 10 * time.Second
 
 func Racer(a, b string) (winner string, error error) {
+    return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
     select {
     case <-ping(a):
         return a, nil
     case <-ping(b):
         return b, nil
-    case <-time.After(10 * time.Second):
+    case <-time.After(timeout):
         return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
     }
 }
+
+
+
+
+
+
+
+
+
