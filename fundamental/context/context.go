4e6cfa7714ef8a57418b5d1168d28c1c7cfb4e20
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-12-04 12:45:52 +0100

    first successfull test of context

---
+++ fundamental/context/context.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+/* 
+Interface to store fetches data.
+*/
+type Store interface {
+	Fetch() string
+}
+
+// Server returns a handler for calling Store.
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
