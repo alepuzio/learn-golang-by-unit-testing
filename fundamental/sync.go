5e789802c6edf02c4db8677b1f3bc3b4f2dd6b54
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-12-04 12:28:50 +0100

    usage of Mutex

--- fundamental/sync.go
+++ fundamental/sync.go
@@ -1,13 +1,31 @@
 package main
 
+
+import "sync"
+
+// Counter will increment a number.
 type Counter struct {
-    value int
+	mu    sync.Mutex //mutux is a lock. 0 means no lock
+	value int
+	//dont declare sync.Mutex as syntax sugar in (1) because: embedded typas are exposed as public interface by  implementation. Very dangerous
 }
 
+/* 
+NewCounter returns a new Counter.
+
+*/
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+// Inc the count.
 func (c *Counter) Inc() {
-    c.value++
+	c.mu.Lock() // NO c.lock() as syntax sugar (1)
+	defer c.mu.Unlock()//releases the lock for next calls
+	c.value++
 }
 
+// Value returns the current count.
 func (c *Counter) Value() int {
-    return c.value
+	return c.value
 }
