c0b5bf7985433b1d0b8a9713d4efdfce76b31ab9
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-12-28 17:29:33 +0100

    fixed the comments

--- application/time/cmd/CLI.go
+++ application/time/cmd/CLI.go
@@ -9,7 +9,9 @@ import (
 	"strings"
 )
 
-// CLI helps players through a game of poker.
+/*
+CLI helps players through a game of poker.
+*/
 type CLI struct {
 	playerStore PlayerStore
 	in          *bufio.Scanner
@@ -17,7 +19,9 @@ type CLI struct {
 	game        Game
 }
 
-// NewCLI creates a CLI for playing poker.
+/*
+NewCLI creates a CLI for playing poker.
+*/
 func NewCLI(in io.Reader, out io.Writer, game Game) *CLI {
 	return &CLI{
 		in:   bufio.NewScanner(in),
@@ -26,16 +30,24 @@ func NewCLI(in io.Reader, out io.Writer, game Game) *CLI {
 	}
 }
 
-// PlayerPrompt is the text asking the user for the number of players.
+/*
+PlayerPrompt is the text asking the user for the number of players.
+*/
 const PlayerPrompt = "Please enter the number of players: "
 
-// BadPlayerInputErrMsg is the text telling the user they did bad things.
+/* 
+BadPlayerInputErrMsg is the text telling the user they did bad things.
+*/
 const BadPlayerInputErrMsg = "Bad value received for number of players, please try again with a number"
 
-// BadWinnerInputMsg is the text telling the user they declared the winner wrong.
+/*
+BadWinnerInputMsg is the text telling the user they declared the winner wrong.
+*/
 const BadWinnerInputMsg = "invalid winner input, expect format of 'PlayerName wins'"
 
-// PlayPoker starts the game.
+/* 
+PlayPoker starts the game.
+*/
 func (cli *CLI) PlayPoker() {
 	fmt.Fprint(cli.out, PlayerPrompt)
 
@@ -58,7 +70,9 @@ func (cli *CLI) PlayPoker() {
 
 	cli.game.Finish(winner)
 }
-
+/**
+@return the winner
+*/
 func extractWinner(userInput string) (string, error) {
 	if !strings.Contains(userInput, " wins") {
 		return "", errors.New(BadWinnerInputMsg)
@@ -66,6 +80,9 @@ func extractWinner(userInput string) (string, error) {
 	return strings.Replace(userInput, " wins", "", 1), nil
 }
 
+/*
+read input
+*/
 func (cli *CLI) readLine() string {
 	cli.in.Scan()
 	return cli.in.Text()
